5bc7a03fbee54280fd8741a8558ecf899b41d15c
link <imshhui@gmail.com>
2017-02-22 16:26:26 +0800

    添加singleton在jdk中的应用例子

--- src/main/java/com/github/xbest/creational/singleton/SafeEarlySingleton.java
+++ src/main/java/com/github/xbest/creational/singleton/SafeEarlySingleton.java
@@ -1,7 +1,8 @@
 package com.github.xbest.creational.singleton;
 
 /**
- * 推荐，虽然耗费一点空间，但是逻辑简单不易出错。
+ * 推荐，虽然耗费一点空间，但是逻辑简单不易出错。<br/>
+ * {@link Runtime#getRuntime()}中就是应用的此类单例模式。
  * Created by link on 2017/2/22.
  */
 public class SafeEarlySingleton {
